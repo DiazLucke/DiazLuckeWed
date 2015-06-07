869d9806a293409481fdd107faa3dc64215c12d6
Daniel Diaz <elguapo2473@Daniel-Diazs-MacBook-Pro.local>
2015-06-06 21:25:30 -0700

    updated engagement party

--- Dates/javascripts/main.js
+++ Dates/javascripts/main.js
@@ -1,11 +1,20 @@
 
 function initialize() {
         var mapCanvas = document.getElementById('map-canvas');
+        var MyLatLang = new google.maps.LatLng(33.771125, -118.116528);
+        
         var mapOptions = {
-          center: new google.maps.LatLng(33.771749, -118.115113),
-          zoom: 18,
-          mapTypeId: google.maps.MapTypeId.HYBRID
+          center: MyLatLang,
+          zoom: 15,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
         }
-        var map = new google.maps.Map(mapCanvas, mapOptions)
+        
+        var map = new google.maps.Map(mapCanvas, mapOptions);
+
+      	var marker = new google.maps.Marker({
+      	position: MyLatLang,
+      	map: map,
+      	title: 'Hello World!'
+  											});
       }
       google.maps.event.addDomListener(window, 'load', initialize);
